87402940001b4f31123b866c8cedaf4b6d2094a4
linswin <linswin@DESKTOP-37A5GDL>
2025-05-17 19:06:10 +0800

    rm unused codes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // app.js
 Array.includes=Array.prototype.includes||function(e){return-1!==this.indexOf(e)};
 App({
-  towxml:require('/towxml/index'),
   onLaunch: function () {
     // 展示本地存储能力
   },
